ca2148afe1aab3e8c8b3d52c9b303d04694b54b3
Francois Grosset <francois.grosset@live.fr>
2025-11-19 20:52:20 +0100

    push du JS

---
+++ meteo.js
@@ -0,0 +1,48 @@
+// Fonction qui va me permettre de récupérer la ville dans le fichier config et sortir le json
+async function fetchConfig() {
+  const response = await fetch("Config/conf.json");
+  return response.json();
+}
+
+//Fonction qui permet de faire appel à l'API
+async function fetchWeather(city) {
+  const apiKey = "7dda1cb9ae5754fcd457dfcc40b4cbb2"
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=fr&appid=${apiKey}&units=metric`
+  
+  const response = await fetch(url);
+  const data = await response.json();
+  // Debug console
+  console.log("Données météo :", data); 
+
+  //appel de la fonction displayWeather pour l'afficher
+  displayWeather(data);
+    }
+
+//Fonction me permettant de modifier mon html et donc ma page en fonction des informations demandées
+function displayWeather(data) {
+  const name = data.name
+  const { description,icon } = data.weather[0]
+  const humidity = data.main.humidity
+  const temp = data.main.temp
+  const speed = data.wind.speed
+  const tempMax = data.main.temp_max
+  const tempMin = data.main.temp_min
+
+  document.querySelector("#city").innerText = name
+  document.querySelector("#icon").src = "https://openweathermap.org/img/wn/" + icon + "@2x.png"
+  document.querySelector("#description").innerText = description
+  document.querySelector("#temp").innerText = temp + "°C"
+  document.querySelector("#humidity").innerText = "Humidité: " + humidity + "%"
+  document.querySelector("#wind").innerText = "Vitesse du vent: " + speed + " km/h"
+  document.querySelector("#tempExt").innerText = "Maxi : " + tempMax + "°C / Mini : " + tempMin + "°C"
+  document.querySelector(".weather").classList.remove("loading");
+    }
+
+async function updateMeteo() {
+  const conf = await fetchConfig();
+  await fetchWeather(conf.ville);
+}
+
+updateMeteo()
+
+setInterval(updateMeteo, 3600000)
